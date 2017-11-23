827cbf2e3cae0bdb67ee94af12b29f450a0577bb
dicrit <dima.bolsunov@gmail.com>
2017-11-23 21:02:48 +0200

    added base model classes

---
+++ InstaBotPrototype/InstaBotPrototype/Models/BoundModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaBotPrototype.Models
+{
+    public class BoundModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string TelegramAccount { get; set; }
+        public string InstagramAccount { get; set; }
+        public string InstagramPassword { get; set; }
+    }
+}
