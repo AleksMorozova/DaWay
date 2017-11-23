e59c957cc8f8a7eb81f5209dc2b3f14d630cbfbc
dicrit <dima.bolsunov@gmail.com>
2017-11-23 18:11:07 +0200

    added queries to create and fill database

---
+++ InstaBotprototype/InsertBaseRows.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Users (Login, Password, Name, Surname, Birth)
+VALUES ('user1','user1password','UserName','UserSurname', '1997-11-16');
+
+INSERT INTO Bounds (UserId, TelegramAccount, InstagramAccount, InstagramPassword)
+VALUES (1,'TELEGA','INSTA_ACC','INSTA_PASS');
+
+INSERT INTO Filters (BoundId, Filter)
+VALUES (1, 'Filter1');
+
+INSERT INTO Filters (BoundId, Filter)
+VALUES (1, 'Filter2');
