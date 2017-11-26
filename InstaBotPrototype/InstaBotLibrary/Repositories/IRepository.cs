ae57c6f2dcad7fb66ea511ae687c50645f6bf947
dicrit <dima.bolsunov@gmail.com>
2017-11-26 19:49:10 +0200

    Filled Interface

--- InstaBotPrototype/InstaBotLibrary/Repositories/IRepository.cs
+++ InstaBotPrototype/InstaBotLibrary/Repositories/IRepository.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using InstaBotLibrary.Models;
 
 namespace InstaBotLibrary.Repositories
 {
     interface IRepository
     {
+        //READ
+        /// <summary>
+        /// check login and password
+        /// </summary>
+        /// <returns>returns user Id, or -1 if it doesn't exist</returns>
+        int verifyUser(AuthorizationModel user);
+        List<BoundModel> getAllBounds(int userId);
+        List<FilterModel> getAllFilters(int filterId);
+        UserModel getUserInfo(int userId);
+
+        //ADD
+        void AddUser(UserModel user);
+        void AddBound(BoundModel bound);
+        void AddFilter(FilterModel filter);
 
 
+        //UPDATE
+        void UpdateUser(UserModel user);
+        void UpdateBound(BoundModel bound);
+        void UpdateFilter(FilterModel filter);
 
-
+        //DELETE
+        void DeleteUser(UserModel user);
+        void DeleteBound(BoundModel bound);
+        void DeleteFilter(FilterModel filter);
 
     }
 }
