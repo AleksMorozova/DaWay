99e2f5a10f7a1622d8e8921c2bbd8e28f8c0ddfb
dicrit <dima.bolsunov@gmail.com>
2017-11-26 19:31:00 +0200

    changed model namespaces

--- InstaBotPrototype/InstaBotLibrary/Models/BoundModel.cs
+++ InstaBotPrototype/InstaBotLibrary/Models/BoundModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InstaBotPrototype.Models
+namespace InstaBotLibrary.Models
 {
     public class BoundModel
     {
--- InstaBotPrototype/InstaBotPrototype/Controllers/ConfigurationController.cs
+++ InstaBotPrototype/InstaBotPrototype/Controllers/ConfigurationController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using InstaBotPrototype.Models;
+using InstaBotLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstaBotPrototype.Controllers
