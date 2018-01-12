44f8d3cbad3d6355f0479cc1faa06486bc8e6b79
VolfsonAlexandr <sancho11alex@gmail.com>
2018-01-12 21:20:29 +0200

    Add file

---
+++ InstaBotPrototype/InstaBotPrototype/ViewLocationExpander.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaBotPrototype
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<String> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<String> viewLocations)
+        {
+            return new[]
+            {
+            "/UI/Views/{1}/{0}.cshtml",
+            "/UI/Views/Shared/{0}.cshtml"
+        };
+        }
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
