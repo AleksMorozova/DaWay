769e9d2a9ca43c6ac371209d4df2c18351d3c842
dicrit <dima.bolsunov@gmail.com>
2018-01-25 12:58:21 +0200

    fixed using in ConfigurationController

--- InstaBotPrototype/InstaBotPrototype/UI/Controllers/ConfigurationController.cs
+++ InstaBotPrototype/InstaBotPrototype/UI/Controllers/ConfigurationController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using InstaBotLibrary.Models;
+using InstaBotLibrary.Configuration;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstaBotPrototype.UI.Controllers
