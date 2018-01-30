877d323575944837d5c01957015aa846495db253
VolfsonAlexandr <sancho11alex@gmail.com>
2018-01-30 18:20:36 +0200

    add authorization on the backend

--- InstaBotPrototype/InstaBotPrototype/UI/Controllers/LoginController.cs
+++ InstaBotPrototype/InstaBotPrototype/UI/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using InstaBotLibrary.User;
+using InstaBotLibrary.Authorization;
 
 namespace InstaBotPrototype.UI.Controllers
 {
@@ -12,11 +14,35 @@ namespace InstaBotPrototype.UI.Controllers
     {
         // GET: Login
         [HttpGet]
-        public ActionResult Login()
+        public ActionResult Login(int? errortype)
         {
+            string [] strarr = {"", "Incorrect login", "Incorrect password"};
+            ViewBag.message = strarr[errortype.GetValueOrDefault(0)];
             return View();
         }
 
-        
+        [HttpPost]
+        public ActionResult Login(string login, string password)
+        {
+            UserRepository repo = new UserRepository();
+            AuthorizationModel auth = new AuthorizationModel();
+            auth = repo.getUserAuthorizationInfo(login);
+            if (auth == null)
+            {
+                return Redirect("/?errortype=1");
+            } 
+            else
+            {
+                if (auth.Password != password)
+                {
+                    return Redirect("/?errortype=2");
+                }
+                else
+                {
+                    return Redirect("/home");
+                }
+            }
+        }
+
     }
 }
