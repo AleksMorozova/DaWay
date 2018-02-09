85b7e56badba1565e5a14870f962576055e6c908
OlgaLisniak <31071114+OlgaLisniak@users.noreply.github.com>
2018-02-09 02:25:42 +0200

    Change LoginController

--- InstaBotPrototype/InstaBotPrototype/UI/Controllers/LoginController.cs
+++ InstaBotPrototype/InstaBotPrototype/UI/Controllers/LoginController.cs
@@ -14,9 +14,41 @@ namespace InstaBotPrototype.UI.Controllers
         [HttpGet]
         public ActionResult Login()
         {
+            ViewBag.message = TempData["message"];
             return View();
         }
 
+
+        [Route("/auth")]
+        [HttpPost]
+        public async Task<ActionResult> AuthAsync(string login, string password)
+        {
+            UserManager userManager = new UserManager();
+
+            if (ModelState.IsValid && userManager.IsLoggedIn(login, password))
+            {
+                HttpContext.Session.SetInt32("user_id", userManager.SessionId(login));
+                await Authenticate(login);
+                return RedirectPermanent("/home");
+            }
+            else
+            {
+                TempData["message"] = "Incorrect login or password";
+                return RedirectPermanent("/login");
+            }
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
         
+
+>>>>>>> 6ab5b77... Change LoginController
     }
 }
