4bea4957b85ab96754a5c83485c1b0a52d620c39
OlgaLisniak <31071114+OlgaLisniak@users.noreply.github.com>
2018-02-09 02:22:59 +0200

    Add UserManager class with interface

---
+++ InstaBotPrototype/InstaBotPrototype/Services/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaBotPrototype.Services
+{
+    public interface IUserManager
+    {
+        bool IsLoggedIn(string login, string password);
+    }
+}
