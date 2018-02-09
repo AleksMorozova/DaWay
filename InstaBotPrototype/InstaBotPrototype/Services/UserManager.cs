babd8dfb152625ddd87a4f4c34a5dabc7106c02b
OlgaLisniak <31071114+OlgaLisniak@users.noreply.github.com>
2018-02-09 02:22:59 +0200

    Add UserManager class with interface

---
+++ InstaBotPrototype/InstaBotPrototype/Services/UserManager.cs
@@ -0,0 +1,37 @@
+﻿using InstaBotLibrary.Authorization;
+using InstaBotLibrary.User;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaBotPrototype.Services
+{
+    public class UserManager : IUserManager
+    {
+        UserRepository userRepository = new UserRepository();
+        AuthorizationModel authModel;
+
+        public bool IsLoggedIn(string login, string password)
+        {
+            authModel = userRepository.getUserAuthorizationInfo(login);
+
+            if (authModel == null)
+            {
+                return false; ;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int SessionId (string login)
+        {
+            authModel = userRepository.getUserAuthorizationInfo(login);
+
+            return authModel.Id;
+        }
+    }
+}
