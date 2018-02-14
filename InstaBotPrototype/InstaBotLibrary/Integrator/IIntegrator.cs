62dc6647b6dbfefbb270e83f0ae8b295c28c0a43
dicrit <dima.bolsunov@gmail.com>
2018-02-14 23:26:35 +0200

    created integrator

---
+++ InstaBotPrototype/InstaBotLibrary/Integrator/IIntegrator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaBotLibrary.Integrator
+{
+    public interface IIntegrator
+    {
+        void Start();
+        void Update();
+    }
+}
---
+++ InstaBotPrototype/InstaBotLibrary/Integrator/Integrator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using InstaBotLibrary.Instagram;
+using InstaBotLibrary.Telegram;
+
+namespace InstaBotLibrary.Integrator
+{
+    public class Integrator
+    {
+        private IInstagramService instagramService;
+        private ITelegramService telegramService;
+        public Integrator(IInstagramService instagram, ITelegramService telegram)
+        {
+            instagramService = instagram;
+            telegramService = telegram;
+        }
+
+
+
+
+        public void Update()
+        {
+            IEnumerable<string> posts = instagramService.GetLatestPosts();
+            foreach (var post in posts)
+            {
+                //if (posts.containsFilters)
+                //{
+                //    telegramService.SendMessage(post);
+                //}
+            }
+
+        }
+
+        public void Start()
+        {
+            int munutesInterval = 1;
+            Timer timer = new Timer(60000 * munutesInterval);
+            timer.AutoReset = true;
+            timer.Elapsed += (a, b) => Update();
+        }
+
+    }
+}
