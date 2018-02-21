64daee4f5fc0883746f761e52342ebf10a0b6ad3
OlgaLisniak <olgalisniak24@gmail.com>
2018-02-22 01:08:58 +0200

    Add class TagsProcessor

---
+++ InstaBotPrototype/InstaBotLibrary/FilterManager/TagsProcessor.cs
@@ -0,0 +1,24 @@
+﻿using InstaBotLibrary.Filter;
+using InstaBotLibrary.Instagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstaBotLibrary.FilterManager
+{
+    public class TagsProcessor
+    {
+        public List<string> TagIntersection(List<FilterModel> filters, Post post)
+        {
+            List<string> inputFilters = new List<string>();
+
+            for (int i = 0; i < filters.Count; i++)
+            {
+                inputFilters.Add(filters[i].Filter);
+            }
+
+            return post.tags.Intersect(inputFilters).ToList();
+        }
+    }
+}
